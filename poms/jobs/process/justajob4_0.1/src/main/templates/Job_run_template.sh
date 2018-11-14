55af200f55d0fb12068e112f7a1263fcd577bc6e
Teddy LEQUETTE <Teddy.LEQUETTE@hardis.fr>
2018-11-14 16:13:36 +0100

    Test Git

---
+++ poms/jobs/process/justajob4_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/antlr-runtime-3.5.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/org.talend.dataquality.parser.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/justajob4_0_1.jar: projet4.justajob4_0_1.justaJob4  "$@" 
